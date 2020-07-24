76da403c2143e35b2e0be3ca4844f0f0416fb049
Mike M <8145849+cibermania5@users.noreply.github.com>
2020-07-24 01:23:29 -0500

    First drop (#1)
    * first upload
    
    * adding gitigoner stuff
    
    * third drop
    
    * deleting

---
+++ models/models.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Casa struct {
+	 ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id.omitempty"`
+	 Casa   string             `json:"casa,omitempty" bson:"casa,omitempty"`
+	 Nombre string             `json:"nombre,omitempty" bson:"nombre,omitempty"`
+	 Cobros[] *Cobro             `json:"cobros,omitempty" bson:"cobros,omitempty"`
+}
+
+type Cobro struct {
+	 Monto float64   `json:"monto,omitempty" bson:"monto,omitempty"`
+	 Causa string    `json:"causa,omitempty" bson:"causa,omitempty"`
+	 Fecha time.Time `json:"fecha,omitempty" bson:"fecha,omitempty"`
+}
+
+type TableroMsg struct{
+	 Mensaje string `json:"mensaje,omitempty" bson:"mensaje,omitempty"`
+}
+
